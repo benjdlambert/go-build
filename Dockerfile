49c9b65300ccfa240344d3042a41cad1a128e8d1
blam <ben.lambert@skybettingandgaming.com>
2016-10-27 16:04:51 +0100

    Fixing functions to make nice

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
 FROM golang:1.7.3-alpine
+RUN apk add --update curl && \
+    rm -rf /var/cache/apk/*
 
+# Add Glide for Dependency Management
+RUN curl https://glide.sh/get | sh
+
+# Create Builder folder
 RUN mkdir /builder
 COPY builder.sh /builder/run.sh
+
+# Make Exec
 RUN chmod +x /builder/run.sh
 
+
+# Set the entrypoint for the dockerfile
 ENTRYPOINT ["/bin/sh", "/builder/run.sh"]
 
--- builder.sh
+++ builder.sh
@@ -3,11 +3,35 @@ set -e
 DIR=$1
 COMMAND=$2
 
+info () {
+    log "\e[1m$1"
+}
+success () {
+    log "\e[32m$1"
+}
+log () {
+    echo -e "[go-build] $1 \e[0m"
+}
+
 # Go to the src directory that is mounted
 cd /go/src/$DIR
 
 if [ "$COMMAND" == "test" ]; then
+    info "Starting go test"
     go test
+    success "Finished go test ✅"
+
+    info "Starting go vet"
+    go vet
+    success "Finished go vet ✅"
+
+    info "Starting go fmt"
     go fmt
+    success "Finished go fmt ✅"
 fi
 
+if [ "$COMMAND" == "build" ]; then
+    info "Installing dependencies with Glide..."
+    glide install
+    success "Finished installing dependencies"
+fi
